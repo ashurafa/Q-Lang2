a4c18900606b003a947f0c09c1eab999f4ad5182
ashurafa <ashurafa@hotmail.com>
2021-07-17 19:57:19 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pYyIBW1DA8":
+        Script1();
+        break;
+      case "6YdKgd9WvqH":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.location.reload();
+}
+
+function Script2()
+{
+  window.location.reload();
+}
+
